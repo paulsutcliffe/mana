5ee7edae16a55d78d1b6fc19fb687820f76bf574
adriel <apinzasm@gmail.com>
2013-11-14 17:53:19 -0500

    Creada la aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mana::Application.config.secret_token = 'f1b0e610c5497ffb2f1f45c9a41fe2c69e0c4b0fafb8fb0eba917d786e5712a2f717aaf4082bea3f2d060a5550b4e118295027ed5289069d9a500bd1b1999249'
